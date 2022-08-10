36416805a4ea722e0af47b9a1fe25ab7ddc047e1
Johyeon Yoon <jalynneyoon@gmail.com>
2022-08-10 15:24:28 +0900

    Chore: Code Edition

--- MakeLocalizableStringFile.py
+++ MakeLocalizableStringFile.py
@@ -14,7 +14,7 @@ def write_localizable_files(localize_dict):
             if idx + 2 < min_row or idx + 2 > max_row:
                 continue
 
-            if '= "";' in string or "=" not in string:
+            if '= "";' in string or " = " not in string:
                 continue
             fp.write(string + "\n")
 
@@ -27,10 +27,12 @@ def add_key(new_path, string):
     with open(new_path + '/' + 'Localizable.strings', "r+") as f:
         d = f.readlines()
         f.seek(0)
-        current_key, current_value = string.split(" = ")
+        current_key = list(string.split(" = "))[0]
 
         for i in d:
-            previous_key, previous_value = i.split(" = ")
+            if '= "";' in i or " = " not in i:
+                continue
+            previous_key = list(i.split(" = "))[0]
 
             if previous_key != current_key:
                 f.write(i)
@@ -40,7 +42,9 @@ def add_key(new_path, string):
         f.truncate()
 
 
-
+# file_name = input("file name: ").rstrip()
+# sheet_name = input("sheet name: ").rstrip()
+# target_path = input("target path: ").rstrip()
 min_row, max_row = map(int, input("text your range: ").split())
 
 file_name = "/Users/yunjohyeon/Downloads/번역배치파일.xlsx"
