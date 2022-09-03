1f221bb866ea80e3e11a627fbfc10fa1640e49f1
JohyeonYoon <jh_yoon@deali.net>
2022-09-03 18:33:58 +0900

    Add sample code

--- WriteLocalizableStringFilesUsingUrl.py
+++ WriteLocalizableStringFilesUsingUrl.py
@@ -1,3 +1,14 @@
 import gspread
 
 # https://github.com/burnash/gspread
+
+gc = gspread.service_account()
+
+# open a sheet
+work_sheet = gc.open("★번역요청").sheet1
+
+# getting all values from a row or a column
+korean_list = work_sheet.col_values(19)
+english_list = work_sheet.col_values(20)
+chinese_list = work_sheet.col_values(21)
+japanese_list = work_sheet.col_values(22)
