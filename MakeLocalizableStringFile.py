cb0c01adcf7ae9870d0b6a2bc3921a7e6ab12d52
JohyeonYoon <jh_yoon@deali.net>
2022-09-05 16:12:00 +0900

    Localizable.strings change writing order

--- MakeLocalizableStringFile.py
+++ MakeLocalizableStringFile.py
@@ -11,33 +11,43 @@ def write_localizable_files(localize_dict):
 
         fp = open(new_path + '/' + 'Localizable.strings', 'a')
         for idx, string in enumerate(value):
-            if idx + 3 < min_row or idx + 3 > max_row:
+            if idx + 2 < min_row or idx + 2 > max_row:
                 continue
 
             if '= "";' in string or " = " not in string:
                 continue
-            fp.write(string + "\n")
 
-            add_key(new_path, string)
+            if check_duplicated_key(new_path, string) == False:
+                fp.write(string + "\n")
 
         print(key + "파일 생성 완료")
 
 
-def add_key(new_path, string):
+def check_duplicated_key(new_path, string):
     with open(new_path + '/' + 'Localizable.strings', "r+") as f:
         d = f.readlines()
         f.seek(0)
         current_key = list(string.split(" = "))[0]
+
+        has_duplication = False
 
         for i in d:
             if '= "";' in i or " = " not in i:
                 continue
             previous_key = list(i.split(" = "))[0]
 
-            if previous_key != current_key:
-                f.write(i)
+            if previous_key == current_key:
+                f.write(string + "\n")
+                has_duplication = True
 
+            else:
+                f.write(i)
         f.truncate()
+
+        if has_duplication:
+            return True
+        return False
+
 
 
 # file_name = input("file name: ").rstrip()
@@ -45,9 +55,9 @@ def add_key(new_path, string):
 # target_path = input("target path: ").rstrip()
 min_row, max_row = map(int, input("text your range: ").split())
 
-file_name = "/Users/yunjohyeon/Downloads/번역배치파일.xlsx"
+file_name = "/Users/johyeonyoon/Downloads/번역배치파일.xlsx"
 sheet_name = "★번역요청"
-target_path = "/Users/yunjohyeon/ssm-mobile-ios/SinsangMarket"
+target_path = "/Users/johyeonyoon/ssm-mobile-ios/SinsangMarket"
 
 
 data = pd.ExcelFile(file_name).parse(sheet_name)
