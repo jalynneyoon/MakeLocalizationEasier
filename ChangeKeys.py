0c565fb7a1db4df8ae1ee941ca3c6814eb44b916
Johyeon Yoon <jalynneyoon@gmail.com>
2022-11-10 19:19:07 +0900

    한 줄에 여러 번역값 존재하는 경우 수정

--- ChangeKeys.py
+++ ChangeKeys.py
@@ -14,10 +14,9 @@ file_name = "/Users/yunjohyeon/Downloads/번역배치파일.xlsx"
 sheet_name = "모웹 통합 시트"
 
 data = pd.ExcelFile(file_name).parse(sheet_name)
-print(data)
-previous_list = list(map(str, data['모바일 키']))
-current_list = list(map(str, data['key']))
-directory_path = "/Users/yunjohyeon/ssm-mobile-ios/SinsangMarket_Some"
+previous_list = list(map(str, data['mk']))
+current_list = list(map(str, data['nk']))
+directory_path = "/Users/yunjohyeon/ssm-mobile-ios/SinsangMarket_Dome"
 
 
 # .swift 파일 내에서 키값들 교체
@@ -30,11 +29,10 @@ def change_key(path):
     for idx, line in enumerate(fp.readlines()):
         new_line = line
         for idx in range(0, len(previous_list)):
-            if '\"' + previous_list[idx] + '\"' in line:
+            if '\"' + previous_list[idx] + '\".localized(hint:' in line:
                 print("** 중복 값 있음: ", previous_list[idx])
-                new_line = line.replace(previous_list[idx], current_list[idx])
+                new_line = new_line.replace('\"' + previous_list[idx] + '\".localized(hint:', '\"' + current_list[idx] + '\".localized(hint:')
                 print("** 변경된 값: ", new_line.lstrip())
-                break
         replacement = replacement + new_line
     fp.close()
 
@@ -47,9 +45,9 @@ def change_key(path):
 for (root, dirs, files) in os.walk(directory_path):
         if len(files) > 0:
             for file_name in files:
-                _, extension = file_name.split(".")
-                if extension != "swift":
+                if ".swift" not in file_name:
                     continue
+                _, extension = file_name.split(".")
                 full_path = root + "/" + file_name
                 change_key(full_path)
 
